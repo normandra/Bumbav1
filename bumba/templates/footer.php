3059c48a8b6a0bf388aae8beb480a0ecd939ebbf
michaelalemu <michael.alemu@gmail.com>
2015-07-21 21:13:39 +0200

    Optimised the floating "+" button

--- bumba/templates/footer.php
+++ bumba/templates/footer.php
@@ -7,9 +7,9 @@
 		<div class="fbtn-inner">
 			<a class="fbtn fbtn-red fbtn-lg" data-toggle="dropdown"><span class="fbtn-text">Links</span><span class="fbtn-ori icon">add</span><span class="fbtn-sub icon">close</span></a>
 			<div class="fbtn-dropdown">
-				<a class="fbtn" href="https://github.com/Daemonite/material" target="_blank"><span class="fbtn-text">Fork me on GitHub</span><span class="fa fa-github"></span></a>
-				<a class="fbtn fbtn-blue" href="https://twitter.com/daemonites" target="_blank"><span class="fbtn-text">Follow Daemon on Twitter</span><span class="fa fa-twitter"></span></a>
-				<a class="fbtn fbtn-alt" href="http://www.daemon.com.au/" target="_blank"><span class="fbtn-text">Visit Daemon Website</span><span class="icon">link</span></a>
+				<a class="fbtn fbtn-yellow" href="#"><span class="fbtn-text">Add new post</span><span class="fa fa-pencil"></span></a>
+				<a class="fbtn fbtn-blue" href="#"><span class="fbtn-text">Add new image</span><span class="fa fa-picture-o"></span></a>
+				<a class="fbtn fbtn-alt" href="#"><span class="fbtn-text">Paste new link</span><span class="icon">link</span></a>
 			</div>
 		</div>
 	</div>
